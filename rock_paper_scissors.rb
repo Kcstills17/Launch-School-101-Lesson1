7828f1b9c5e014eb7ff9b39d2eeb8db8582d3144
kcstills17 <zanelee28@yahoo.comm>
2022-09-06 14:02:49 -0400

    I really feel it in my bones now. I will finish yougit add -A

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -19,37 +19,44 @@ end
 def win?(first, second)
   if first == second
     prompt("You Tie")
+
+
   elsif action = RPS[[first, second]]
     prompt("Good Work! #{first} completely #{action} #{second}")
-    first
+    [first, second]
+
+
+
   elsif action = RPS[[second, first]]
     prompt("Oh No! #{second}  #{action} #{first}. You've still got this!")
-    second
+    [second, first]
+
+
   end
-  second
+
 end
 
-def display_score(user,cpu)
+def display_score(user, cpu)
   prompt("Alright. Now you get the jigg. Make your pick!")
   cpu_score = 0
   your_score = 0
   loop do
     user = gets.chomp
-    cpu = VALID_CHOICES.sample
+    cpu = 'p'
     if VALID_CHOICES.include?(user) == false
       prompt("please enter a valid input")
-    elsif win?(user,cpu)
+
+     elsif win?(user, cpu) == [user, cpu]
       your_score += 1
       break if your_score == 3
       prompt("your score #{your_score} opponent's score #{cpu_score}")
-    elsif win?(cpu,user)
+    elsif win?(cpu, user) == [cpu, user]
       cpu_score += 1
       break if cpu_score == 3
       prompt("your score #{your_score} opponent's score #{cpu_score}")
     else
       prompt("Your choices are equal.")
     end
-
   end
   prompt("Final Score : #{your_score} to : #{cpu_score}")
 end
@@ -62,7 +69,7 @@ loop do
   prompt("Welcome to our Upgraded Rock Paper Scissors Game! Please enter anything to begin ")
   enter_game = gets.chomp
   loop do
-    prompt("Enter 'r', 'p', 's', 'sp', or 'l' to pick your choice. This is a practice round and you will not be scored. ")
+    prompt("Practice: enter r, p, s, sp, or l. This will not be scored ")
     user_choice = gets.chomp
     cpu_choice = VALID_CHOICES.sample
     player_score = 0
@@ -76,5 +83,4 @@ loop do
   prompt("Thank You for enjoying our game. Please notify us if you would like to play again")
   play_again = gets.chomp
   break unless play_again.start_with?('y'.downcase)
-
 end
