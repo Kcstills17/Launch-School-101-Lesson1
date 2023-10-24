2e50c4240789fdb99a24bfda8cf98c6e060faf21
kcstills17 <zanelee28@yahoo.comm>
2023-10-24 14:29:45 -0400

    finished todo list

--- RB175/todo_list/todo.rb
+++ RB175/todo_list/todo.rb
@@ -12,15 +12,52 @@ before do
   session[:lists] ||= []
 end
 
+not_found do
+  redirect "/lists"
+end
 
+helpers do
+  def list_complete?(list)
+    list[:todos].all? { |todo| todo[:completed] == true } && !todos_empty?(list)
+  end
+
+  def todos_empty?(list)
+    list[:todos].empty?
+  end
+
+  def completed_todos(lists)
+    lists[:todos].count { |todo| todo[:completed] == true }
+  end
+
+  def todo_complete?(todo)
+    todo[:completed] == true
+  end
+
+  def todos_size(list)
+    list[:todos].size
+  end
+
+  def sort_lists(lists)
+    complete_lists, incomplete_lists = lists.partition { |list| list_complete?(list) }
+
+    incomplete_lists.each { |list| yield list, lists.index(list) }
+    complete_lists.each { |list| yield list, lists.index(list) }
+  end
+
+  def sort_todos(todos)
+    complete_todos, incomplete_todos = todos.partition { |todo| todo_complete?(todo) }
+
+    incomplete_todos.each { |todo| yield todo, todos.index(todo) }
+    complete_todos.each { |todo| yield todo, todos.index(todo) }
+  end
+end
 
 def error_for_list_name(name)
   if !(1..100).cover? name.size
-  "The list name must be between 1 and 100 characters."
-  elsif session[:lists].any? {|list| list[:name] == name}
-   "Please choose a list that has not already been created."
+    "The list name must be between 1 and 100 characters."
+  elsif session[:lists].any? { |list| list[:name] == name }
+    "Please choose a list that has not already been created."
   end
-
 end
 
 def error_for_todo(todo)
@@ -29,7 +66,6 @@ def error_for_todo(todo)
   end
 end
 
-
 get "/" do
   redirect "/lists"
 end
@@ -37,71 +73,73 @@ end
 get "/lists" do
   @lists = session[:lists]
 
+  # @any_todos_completed = @lists[:todos].any? {|todo| todo[:completed] == true }
+
+  # @all_todos_completed = @lists[:todos].all? {|todo| todo[:completed] == true }
+  # @todos_empty = @lists[:todos].empty?
+
   erb :lists, layout: :layout
 end
 
 # render new list form
 get "/lists/new" do
- erb :new_list, layout: :layout
+  erb :new_list, layout: :layout
 end
 
 get "/lists/:number" do
   @list_id = params[:number].to_i
-@list = session[:lists][@list_id]
-erb :single_list, layout: :layout
+  @list = session[:lists][@list_id]
+
+  erb :single_list, layout: :layout
 end
 
-#edit existing todo list
+# edit existing todo list
 
 get "/lists/:number/edit" do
   id = params[:number].to_i
   @list = session[:lists][id]
-erb :edit_list, layout: :layout
+  erb :edit_list, layout: :layout
 end
 
 # create a new list
 post "/lists" do
   list_name = params[:list_name].strip
 
-if error = error_for_list_name(list_name)
-  session[:error] = error
-  erb :new_list, layout: :layout
+  if (error = error_for_list_name(list_name))
+    session[:error] = error
+    erb :new_list, layout: :layout
   else
-    session[:lists] << {name: list_name, todos: [] }
+    session[:lists] << { name: list_name, todos: [] }
     session[:success] = "The list has been created"
     redirect "/lists"
   end
-
 end
 
-#update list
+# update list
 post "/lists/:number" do
   list_name = params[:list_name].strip
   id = params[:number].to_i
   @list = session[:lists][id]
 
-if error = error_for_list_name(list_name)
-  session[:error] = error
-  erb :edit_list, layout: :layout
+  if (error = error_for_list_name(list_name))
+    session[:error] = error
+    erb :edit_list, layout: :layout
+
   else
     @list[:name] = list_name
     session[:success] = "The list has been Updated"
-    redirect "/lists/#{:number}"
+    redirect "/lists/#{id}"
   end
-
-
 end
 
 post "/lists/:number/delete" do
   puts "hello world"
   id = params[:number].to_i
-   session[:lists].delete_at(id)
+  session[:lists].delete_at(id)
 
-   session[:success] = "The list has been Deleted"
-   redirect "/lists"
-
+  session[:success] = "The list has been Deleted"
+  redirect "/lists"
 end
-
 
 # create a todo
 post "/lists/:list_id/todos" do
@@ -114,15 +152,13 @@ post "/lists/:list_id/todos" do
     session[:error] = error
     erb :single_list, layout: :layout
   else
-    @list[:todos] << {name: text, completed: false}
+    @list[:todos] << { name: text, completed: false }
     session[:success] = "The todo has been added to the list"
     redirect "lists/#{@list_id}"
   end
-
-
 end
 
-#delete todos from specific lists
+# delete todos from specific lists
 post "/lists/:list_id/todos/:id/delete" do
   @list_id = params[:list_id].to_i
   @list = session[:lists][@list_id]
@@ -133,7 +169,7 @@ post "/lists/:list_id/todos/:id/delete" do
   redirect "/lists/#{@list_id}"
 end
 
-#determine if todo is complete or not. AKA update the status of todo
+# determine if todo is complete or not. AKA update the status of todo
 post "/lists/:list_id/todos/:id" do
   @list_id = params[:list_id].to_i
   @list = session[:lists][@list_id]
@@ -143,13 +179,19 @@ post "/lists/:list_id/todos/:id" do
 
   session[:success] = "The todo has succesfully been Updated!"
   redirect "/lists/#{@list_id}"
-
 end
 
+# complete all todos in the list
+post "/lists/:list_id/complete_all_todos" do
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+  @list[:todos].each { |todo| todo[:completed] = true }
+  session[:success] = "All Todo tasks have been completed!"
 
+  redirect "lists/#{@list_id}"
+end
 
 set :session_secret, SecureRandom.hex(32)
-
 
 # sequence of all http method neccessary
 =begin
@@ -160,5 +202,3 @@ GET /lists/1 -> view a specific list
 GET /users -> view a list of all users
 GET /users/1 -> view a specific user
 =end
-
-
